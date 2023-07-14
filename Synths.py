169f4433072495008248d81d800c39df45a928b2
Javiera Zamorano Osorio <javizamoso@gmail.com>
2023-07-14 18:37:29 +0100

    Splitting code into libraries

---
+++ Synths.py
@@ -0,0 +1,11 @@
+import torch
+import math
+
+
+def damped_synth(pitches, amplitudes, damping, sampling_rate):
+    assert pitches.shape[-1] == amplitudes.shape[-1]
+    indices = torch.arange(pitches.size(1)).unsqueeze(0).unsqueeze(-1)
+    damper = torch.exp(- damping.abs() * indices)
+    omegas = torch.cumsum(2 * math.pi * pitches / sampling_rate, 1)
+    signal = (torch.cos(omegas) * amplitudes * damper).sum(-1, keepdim=True)
+    return signal
