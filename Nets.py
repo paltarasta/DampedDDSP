Fixing where softmax in network was being taken over the wrong dimension

Nets.py
@@ -185,12 +185,13 @@ class DampingMapping(nn.Module):
     self.damping_dense = nn.Linear(9*1024, 100)
     self.amp_scale = h.exp_sigmoid
     self.register_buffer("scale", torch.logspace(np.log2(20), np.log2(8000), 64, base=2.0))
-    self.softmax = nn.Softmax(dim=2)
+    self.softmax = nn.Softmax(dim=3)
 
   def forward(self, x):
     out = self.resnet(x)
     out = rearrange(out, 'z a b c -> z b c a')
     out = torch.reshape(out, (out.shape[0], 125, 9*1024))
+    print('out',out.shape)
 
     frequency = self.frequency_dense(out)
     frequency = rearrange(frequency, 'b t (k f) -> b t k f', f=64)
@@ -221,12 +222,12 @@ class SinMapToFrequency(nn.Module):
     self.amplitude_dense = nn.Linear(9*1024, 100)
     self.amp_scale = h.exp_sigmoid
     self.register_buffer("scale", torch.logspace(np.log2(20), np.log2(8000), 64, base=2.0))
-    self.softmax = nn.Softmax(dim=2)
+    self.softmax = nn.Softmax(dim=3)
 
   def forward(self, x):
     out = self.resnet(x)
     out = rearrange(out, 'z a b c -> z b c a')
-    out = torch.reshape(out, (out.shape[0], 126, 9*1024))
+    out = torch.reshape(out, (out.shape[0], 125, 9*1024))
 
     frequency = self.frequency_dense(out)
     frequency = rearrange(frequency, 'b t (k f) -> b t k f', f=64)

Pretrain.py
@@ -6,12 +6,11 @@ import auraloss as al
 from tqdm import tqdm
 import Synths as s
 from einops import rearrange
-import matplotlib.pyplot as plt
 import os
 import Losses as l
 from torch.utils.tensorboard import SummaryWriter
 
-writer = SummaryWriter()
+writer = SummaryWriter('pretrain')
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #print(f"Running on device: {device}")
@@ -21,13 +20,13 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #################################################################################################
 
 ### Load tensors and create dataloader ###
-MELS = torch.load('SavedTensors/melsynth.pt')
+MELS = torch.load('SavedTensors/melsynth.pt')[:,:,:125,:]
 Y = torch.load('SavedTensors/ysynth.pt')
-sin_amps_target = torch.load('SavedTensors/sin_amps.pt')
-sin_freqs_target = torch.load('SavedTensors/sin_freqs.pt')
-harm_amp_target = torch.load('SavedTensors/harm_amp.pt')
-harm_dist_target = torch.load('SavedTensors/harm_dist.pt')
-f0_hz_target = torch.load('SavedTensors/f0_hz.pt')
+sin_amps_target = torch.load('SavedTensors/sin_amps.pt')[:,:125,:]
+sin_freqs_target = torch.load('SavedTensors/sin_freqs.pt')[:,:125,:]
+harm_amp_target = torch.load('SavedTensors/harm_amp.pt')[:,:125,:]
+harm_dist_target = torch.load('SavedTensors/harm_dist.pt')[:,:125,:]
+f0_hz_target = torch.load('SavedTensors/f0_hz.pt')[:,:125,:]
 
 if __name__ == "__main__":
   synth_dataset = h.SyntheticDataset(MELS, Y, harm_amp_target, harm_dist_target, f0_hz_target, sin_amps_target, sin_freqs_target)
@@ -43,14 +42,14 @@ if __name__ == "__main__":
 
   sin_criterion = al.freq.MultiResolutionSTFTLoss(fft_sizes=[1024, 2048, 512])#.cuda()
   harm_criterion = al.freq.MultiResolutionSTFTLoss(fft_sizes=[1024, 2048, 512])#.cuda()
-  consistency_criterion = l.KDEConsistencyLoss#.cuda()
+  consistency_criterion = l.KDEConsistencyLoss
   self_supervision_criterion = l.HarmonicConsistencyLoss#.cuda()
 
   params = list(sin_encoder.parameters()) + list(harm_encoder.parameters())
   optimizer = torch.optim.Adam(params, lr=0.0003)
 
   # Training loop
-  num_epochs = 2
+  num_epochs = 5
   i = 0
 
   for epoch in range(num_epochs):
@@ -63,34 +62,29 @@ if __name__ == "__main__":
         print(audio.shape, 'THE TARGETS SHAPE')
         print(mels.shape, 'THE INPUTS SHAPE')
 
-        #mels = mels.cuda()
-        #audio = audio.cuda()
-        #harm_amp_t = harm_amp_t.cuda()
-        #harm_dist_t = harm_dist_t.cuda()
-        #f0_hz_t = f0_hz_t.cuda()
-        #sin_amps_t = sin_amps_t.cuda()
-        #sin_freqs_t = sin_freqs_t.cuda()
+        mels = mels#.cuda()
+        audio = audio#.cuda()
+        glob_amp_t = glob_amp_t#.cuda()
+        harm_dist_t = harm_dist_t#.cuda()
+        f0_hz_t = f0_hz_t#.cuda()
+        sin_amps_t = sin_amps_t#.cuda()
+        sin_freqs_t = sin_freqs_t#.cuda()
 
-        if i > 4:
-          break
-            
         #Sinusoisal encoder
         sin_freqs, sin_amps = sin_encoder(mels)
-        print('post sin encoder')
 
         #Sinusoidal synthesiser
         sin_signal = s.sinusoidal_synth(sin_freqs, sin_amps, 16000)
         sin_signal = rearrange(sin_signal, 'a b c -> a c b')
-        print('post sin synth')
 
         #Sinusoidal reconstruction loss
         sin_recon_loss = sin_criterion(sin_signal, audio.unsqueeze(0))
+        writer.add_scalar('Sinusoidal Recon Loss/Pretrain', sin_recon_loss, i)
 
         #Harmonic encoder
         sin_freqs = sin_freqs.detach() #detach gradients before they go into the harmonic encoder
         sin_amps = sin_amps.detach()
         glob_amp, harm_dist, f0 = harm_encoder(sin_freqs, sin_amps)
-        print('post harm encoder')
 
         #Reconstruct audio from harmonic encoder results
         harmonics = h.get_harmonic_frequencies(f0) #need this to then do the sin synth - creates a bank of 100 sinusoids
@@ -103,13 +97,11 @@ if __name__ == "__main__":
 
         #Harmonic reconstruction loss
         harm_recon_loss = harm_criterion(harm_signal, audio.unsqueeze(0))
-        print('post harm loss')
+        writer.add_scalar('Harmonic Recon Loss/Pretrain', harm_recon_loss, i)
 
         #Consistency loss
         consistency_loss = consistency_criterion(harm_amps, harmonics, sin_amps, sin_freqs) #do i need to include the damping here?
-        print('sin loss', sin_recon_loss)
-        print('harm loss', harm_recon_loss)
-        print('consistency loss', consistency_loss)
+        writer.add_scalar('Consistency Loss/Pretrain', consistency_loss, i)
 
         #Self-supervision loss
         ss_loss = self_supervision_criterion(glob_amp,
@@ -123,29 +115,27 @@ if __name__ == "__main__":
                                              sin_freqs,
                                              sin_freqs_t
                                              )
-        print('self supervision', ss_loss)
+        writer.add_scalar('SSLoss/Pretrain', ss_loss, i)
 
         #Total loss
         total_loss = sin_recon_loss + harm_recon_loss + (0.1 * consistency_loss) + ss_loss
-        print('after losses all summed', total_loss, type(total_loss), total_loss.shape)
 
-        writer.add_scalar('Loss/train', total_loss, i)
+        writer.add_scalar('Loss/Pretrain', total_loss, i)
 
-        print('starting backwards')
         optimizer.zero_grad()
         total_loss.backward()
-        print('backwards done')
         optimizer.step()
-        print('finished')
 
         running_loss.append(total_loss.item())
         tepoch.set_description_str(f"{epoch}, loss = {total_loss.item():.4f}", refresh=True)
 
+        if i%100 == 0:
+          # Save a checkpoint
+          torch.save(sin_encoder.state_dict(), f'Checkpoints/sin_encoder_ckpt_{epoch}.pt')
+          torch.save(harm_encoder.state_dict(), f'Checkpoints/harm_encoder_ckpt_{epoch}.pt')
+
         i += 1
 
-      # Save a checkpoint
-      torch.save(sin_encoder.state_dict(), f'Checkpoints/sin_encoder_ckpt_{epoch}.pt')
-      torch.save(harm_encoder.state_dict(), f'Checkpoints/harm_encoder_ckpt_{epoch}.pt')
 
   writer.flush()
   writer.close()